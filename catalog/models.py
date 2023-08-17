7d75407f8ef29f164344d37164165fe52b4300cb
Quan Dang <danganhquan246@gmail.com>
2023-08-17 16:43:06 +0700

    Django Tutorial Part 4: Django admin site

--- catalog/models.py
+++ catalog/models.py
@@ -27,6 +27,12 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ','.join(genre.name for genre in self.genre.all()[:3])
+        
+        display_genre.short_description = 'Genre'
+
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
@@ -70,3 +76,4 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
+
