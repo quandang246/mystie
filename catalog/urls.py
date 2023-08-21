d3c22ad35fc0ac15e037cb5c1d7f8365ddd34f50
Quan Dang <danganhquan246@gmail.com>
2023-08-21 15:30:13 +0700

    Django Tutorial Part 5: Creating our home page

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.index, name = 'index'),
+    path('set-language/<str:language_code>/', views.set_language, name='set_language'),
 ]
