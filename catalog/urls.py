b13880ca422ab56456f09ba7eaf1248000d59e05
Quan Dang <danganhquan246@gmail.com>
2023-08-21 17:57:18 +0700

    Django Tutorial Part 8: User authentication and permission.

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,3 +8,5 @@ urlpatterns = [
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
+
+urlpatterns += [path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),]
--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,7 @@ from typing import Any, Dict
 from django.shortcuts import render
 from django.views import generic
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -52,3 +53,14 @@ class BookDetailView(generic.DetailView):
 def book_detail_view(request, primary_key):
     book = get_object_or_404(Book, pk=primary_key)
     return render(request, 'catalog/book_detail.html', context={'book': book})
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """
+    Generic class-based view listing books on loan to current user.
+    """
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
